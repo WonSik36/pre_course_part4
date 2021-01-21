5085bd19b2909175ca6b2ad4dcb318d56bde527e
WonSik36 <cws0173@gmail.com>
2021-01-21 10:42:01 +0900

    First Commit

---
+++ src/main/java/org/zerock/mvreview/MvreviewApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mvreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvreviewApplication.class, args);
+	}
+
+}
